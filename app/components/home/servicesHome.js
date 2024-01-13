6d0bd8d50d472d926a40df528a3a67330f979ca8
Javier Garcia Carrillo <50500251+javiergcw@users.noreply.github.com>
2024-01-13 14:48:07 -0500

    services completed

--- app/components/home/servicesHome.js
+++ app/components/home/servicesHome.js
@@ -4,17 +4,18 @@ import React from 'react';
 
 const ServicesHome = ({ images }) => {
     return (
-        <div className="flex flex-wrap justify-center md:justify-start">
+        <div className="bg-white h-full justify-center flex w-full lg:flex-row  lg:justify-start lg:h-40 items-center">
             {images.map((image, index) => (
-                <div key={index} className="group relative m-4">
+                <div key={index} className="group relative m-0 w-full">
                     <div className="flex justify-center">
                         <a href={image.href}>
-                            <img src={image.src} alt={image.alt} className="block" />
+                            <img src={image.src} alt={image.alt} className="block w-full" />
                         </a>
                     </div>
-                    <div className="relative w-full border-b border-gray-300 group-hover:border-blue-500 mt-2">
-                        <div className="hidden group-hover:block absolute left-1/2 -top-2 transform -translate-x-1/2 w-0 h-0 border-l-4 border-r-4 border-b-4 border-transparent border-b-blue-500"></div>
+                    <div className="relative w-full border-0 group-hover:border-2 group-hover:border-[#2991D6] mt-2">
+                        <div className="hidden group-hover:block absolute left-1/2 top-[calc(100%-0.3rem)] transform -translate-x-1/2 w-0 h-0 border-l-4 border-r-4 border-b-4 border-transparent group-hover:border-b-[#2991D6]"></div>
                     </div>
+
                 </div>
             ))}
         </div>
@@ -23,25 +24,3 @@ const ServicesHome = ({ images }) => {
 
 export default ServicesHome;
 
-/* import React from 'react';
-
-const ServicesHome = ({ images }) => {
-    return (
-        <div className="flex flex-wrap justify-center md:justify-start">
-            {images.map((image, index) => (
-                <div key={index} className="group relative m-4">
-                    <div className="flex justify-center">
-                        <a href={image.href}>
-                            <img src={image.src} alt={image.alt} className="block" />
-                        </a>
-                    </div>
-                    <div className="w-full border-b border-gray-300 group-hover:border-blue-500 mt-2">
-                        <div className="hidden group-hover:block absolute left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-4 border-r-4 border-b-4 border-transparent border-b-blue-500" style={{ bottom: '-6px' }}></div>
-                    </div>
-                </div>
-            ))}
-        </div>
-    );
-};
-
-export default ServicesHome; */
