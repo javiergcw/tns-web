ad67f406765b235e9d68edc7e021232379688d1e
Javier Garcia Carrillo <50500251+javiergcw@users.noreply.github.com>
2024-01-13 16:10:59 -0500

    feat: :art: configuracion inicial de blogs

--- app/components/home/servicesHome.js
+++ app/components/home/servicesHome.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 const ServicesHome = ({ images }) => {
     return (
-        <div className="bg-white h-full justify-center flex w-full lg:flex-row  lg:justify-start lg:h-40 items-center">
+        <div className="bg-white justify-center flex w-full lg:flex-row lg:justify-start lg:h-40 items-center">
             {images.map((image, index) => (
                 <div key={index} className="group relative m-0 w-full">
                     <div className="flex justify-center">
@@ -15,6 +15,7 @@ const ServicesHome = ({ images }) => {
                     <div className="relative w-full border-0 group-hover:border-2 group-hover:border-[#2991D6] mt-2">
                         <div className="hidden group-hover:block absolute left-1/2 top-[calc(100%-0.3rem)] transform -translate-x-1/2 w-0 h-0 border-l-4 border-r-4 border-b-4 border-transparent group-hover:border-b-[#2991D6]"></div>
                     </div>
+
 
                 </div>
             ))}
--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import ServicesHome from "./components/home/servicesHome";
 import CalendarScheduleHome from "./components/home/calendarScheduleHome";
 import TopHeader from "./components/home/header/topHeader";
 import Navbar from "./components/home/navbar";
+import BlogsSection from "./components/home/blogsSection";
 
 
 export default function Home() {
@@ -21,12 +22,38 @@ export default function Home() {
     { href: 'https://google.com', src: 'images/services/services.png', alt: 'Services' },
     { href: 'https://google.com', src: 'images/services/beam.png', alt: 'Beam' },
   ];
+
+
+  const blogList = [
+    {
+      date: "December 12,2023",
+      imageUrl: 'images/blogs/blog1.jpg',
+      title: "Blog Title 1"
+    },
+    {
+      date: "October 4,2023",
+      imageUrl: "images/blogs/blog2.jpeg",
+      title: "Blog Title 2"
+    },
+    {
+      date: "2024-01-03",
+      imageUrl: "images/blogs/blog3.jpg",
+      title: "Blog Title 3"
+    },
+    {
+      date: "2024-01-04",
+      imageUrl: "images/blogs/blog4.jpeg",
+      title: "Blog Title 4"
+    },
+  ];
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between ">
       <TopHeader />
       <Navbar />
       <BannerCarousel imagePaths={imagePaths} />
       <ServicesHome images={servicesList} />
+      <BlogsSection blogs={blogList} />
       <CalendarScheduleHome />
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
 
