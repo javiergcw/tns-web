dff2968ec64ba9c753ff74b307bdd4a8496288f7
gabox07 <gabgamardo25@gmail.com>
2024-02-01 16:18:25 -0500

    cambias adicionales

--- app/components/somos/bilingue.js
+++ app/components/somos/bilingue.js
@@ -1,20 +1,31 @@
 export default function Bilingue() {
     return (
-      <>
-        <div class="contenedor">
-            <div class="izquierda">
-                <h3>Propuesta Bilingüe</h3>
-                <h4>Por qué apostamos a una propuesta bilingüe</h4>
-                <p className='parrafos'>Hoy más que nunca resulta imprescindible aprender inglés. Es indiscutible: el inglés se ha convertido en el idioma global de comunicación por excelencia. Es indispensable cuando viajamos, ya sea por turismo o negocios.
-                <br></br>
-                En estos casos, es necesario hablar inglés para ir de compras, aprender inglés para poder tomar un tren sin perderse o para pedir la cuenta en un restaurante donde hablan solamente inglés. Además, la mayoría de las páginas de Internet, prensa mundial e información científica se encuentran en idioma inglés.
-                <br></br>
-                Nuestra institución le apunta al idioma inglés como una de las herramientas que le damos al estudiante para alcanzar sus metas en la vida y para incursionar en el multilingüismo, este año la básica secundaria cuenta con clases de francés.</p>
-            </div>
-            <div class="derecha">
-                <img className="img_size"  src="http://www.thenewschool.edu.co/wp-content/uploads/2015/09/thumbnail_fadfs.jpg" alt="" srcset=""></img>
+        <div className="backgroundImagefaith">
+            <div className="contenedor flex justify-center items-center min-h-screen">
+                <div className="flex flex-col md:flex-row max-w-6xl mx-auto items-center">
+
+                    {/* Sección de Texto */}
+                    <div className="md:w-1/2 text-center md:text-left px-4">
+                        <h3 className="text-4xl text-[#444444] mb-4">Propuesta Bilingüe</h3>
+                        <h4 className="text-3xl text-[#444444] mb-4">Por qué apostamos a una propuesta bilingüe</h4>
+                        <p className="text-gray-500 text-[13px]">
+                            Hoy más que nunca resulta imprescindible aprender inglés. Es indiscutible: el inglés se ha convertido en el idioma global de comunicación por excelencia. Es indispensable cuando viajamos, ya sea por turismo o negocios.
+                            <br></br>
+                            En estos casos, es necesario hablar inglés para ir de compras, aprender inglés para poder tomar un tren sin perderse o para pedir la cuenta en un restaurante donde hablan solamente inglés. Además, la mayoría de las páginas de Internet, prensa mundial e información científica se encuentran en idioma inglés.
+                            <br></br>
+                            Nuestra institución le apunta al idioma inglés como una de las herramientas que le damos al estudiante para alcanzar sus metas en la vida y para incursionar en el multilingüismo, este año la básica secundaria cuenta con clases de francés.
+                        </p>
+                    </div>
+
+                    {/* Sección de Imagen */}
+                    <div className="md:w-1/2 flex justify-center">
+                        <img className="md:w-[420px]"
+                             src="http://www.thenewschool.edu.co/wp-content/uploads/2015/09/thumbnail_fadfs.jpg"
+                             alt="Descripción de la imagen">
+                        </img>
+                    </div>
+                </div>
             </div>
         </div>
-      </>
     );
-  }
+}
--- app/components/somos/promueve.js
+++ app/components/somos/promueve.js
@@ -1,44 +1,85 @@
 export default function Promueve() {
     return (
-      <>
-        <div class="contenedor ">
-            <div class="izquierda ">
-                <h3>La Escuela Nueva promueve</h3>
-                <ul>
-                    <li>El respeto por el ritmo de aprendizaje.</li>
-                    <li>Procesos dinámicos, democráticos, participativos, activos y creativos.</li>
-                    <li>La aplicación de guías de aprendizaje por áreas, rincones de trabajo, bibliobancos, buzón de amigos y uso de materiales didácticos.</li>
-                    <li>Docentes facilitadores del proceso enseñanza-aprendizaje, en el marco de una relación horizontal en la cual ambos se complementan, comparten experiencias y desarrollan una clase lúdica, creativa y dinámica.</li>
-                    <li>La retroalimentación permanente del estudiante para aclarar dudas e identificar falencias que le permitan al docente diseñar estrategias pedagógicas orientadas a mejorar y a valorar sus logros.</li>
-                    <li>La participación democrática de los estudiantes a través del gobierno escolar y de los líderes estudiantiles, permitiéndoles expresar sus necesidades e intereses.</li>
-                    <li>El trabajo cooperativo o en equipo, con el fin de favorecer la autorregulación del aprendizaje, la asunción de responsabilidades, la participación de todos, las habilidades comunicativas, la ayuda mutua, el respeto, la empatía. El trabajo en equipo es, además, una de las mejores estrategias para abordar la diversidad del aula y caminar hacia una escuela verdaderamente inclusiva.</li>
-                    <li>Una educación fundamentada en los intereses, potencialidades y necesidades de los alumnos.</li>
-                </ul>
-                
+        <>
+            <div className="contenedor flex flex-col md:flex-row justify-center items-center min-h-screen">
+                {/* Sección Izquierda */}
+                <div className="izquierda md:w-1/2 text-center md:text-left px-4">
+                    <h3 className="text-4xl text-[#444444] mb-4">La Escuela Nueva promueve</h3>
+                    <ul className="text-gray-500 text-[13px] list-disc list-inside">
+                        <li>El respeto por el ritmo de aprendizaje.</li>
+                        <li>Procesos dinámicos, democráticos, participativos, activos y creativos.</li>
+                        <li>La aplicación de guías de aprendizaje por áreas, rincones de trabajo, bibliobancos, buzón de
+                            amigos y uso de materiales didácticos.
+                        </li>
+                        <li>Docentes facilitadores del proceso enseñanza-aprendizaje, en el marco de una relación
+                            horizontal en la cual ambos se complementan, comparten experiencias y desarrollan una clase
+                            lúdica, creativa y dinámica.
+                        </li>
+                        <li>La retroalimentación permanente del estudiante para aclarar dudas e identificar falencias
+                            que le permitan al docente diseñar estrategias pedagógicas orientadas a mejorar y a valorar
+                            sus logros.
+                        </li>
+                        <li>La participación democrática de los estudiantes a través del gobierno escolar y de los
+                            líderes estudiantiles, permitiéndoles expresar sus necesidades e intereses.
+                        </li>
+                        <li>El trabajo cooperativo o en equipo, con el fin de favorecer la autorregulación del
+                            aprendizaje, la asunción de responsabilidades, la participación de todos, las habilidades
+                            comunicativas, la ayuda mutua, el respeto, la empatía. El trabajo en equipo es, además, una
+                            de las mejores estrategias para abordar la diversidad del aula y caminar hacia una escuela
+                            verdaderamente inclusiva.
+                        </li>
+                        <li>Una educación fundamentada en los intereses, potencialidades y necesidades de los alumnos.
+                        </li>
+                    </ul>
+                </div>
+
+                {/* Sección Derecha */}
+                <div className="derecha md:w-1/2 text-center md:text-left px-4">
+                    <h3 className="text-4xl text-[#444444] mb-4">El Pensamiento Sistémico promueve</h3>
+                    <ul className="text-gray-500 text-[13px] list-disc list-inside">
+                        <li>La observación deliberada y amplia de los acontecimientos, con el fin de propiciar una
+                            visión integral de las situaciones y facilitar la toma de decisiones de acuerdo a la
+                            capacidad crítica y equitativa.
+                        </li>
+                        <li>Pautas de conducta que permitan el desarrollo de modelos mentales de una sana convivencia.
+                        </li>
+                        <li>La oportunidad de mejorar sus dificultades y errores, fundamentados en que son una valiosa
+                            oportunidad de aprendizaje.
+                        </li>
+                        <li>La integralidad del ser humano en el desarrollo del proceso formativo.</li>
+                        <li>Un modelo cientificista que trabaja la estructura curricular sobre líneas de
+                            investigación.
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div class="derecha ">
-                <h3>El Pensamiento Sistémico promueve</h3>
-                <ul>
-                    <li>La observación deliberada y amplia de los acontecimientos, con el fin de propiciar una visión integral de las situaciones y facilitar la toma de decisiones de acuerdo a la capacidad crítica y equitativa.</li>
-                    <li>Pautas de conducta que permitan el desarrollo de modelos mentales de una sana convivencia.</li>
-                    <li>La oportunidad de mejorar sus dificultades y errores, fundamentados en que son una valiosa oportunidad de aprendizaje.</li>
-                    <li>La integralidad del ser humano en el desarrollo del proceso formativo.</li>
-                    <li>Un modelo cientificista que trabaja la estructura curricular sobre líneas de investigación.</li>
+
+            {/* Sección Adicional (sin identificador) */}
+            <div className="additional text-center md:text-left px-4 mt-8 md:mt-0 md:ml-4">
+                <h3 className="text-4xl text-[#444444] mb-4">El Aprendizaje Significativo promueve:</h3>
+                <ul className="text-gray-500 text-[13px] list-disc list-inside">
+                    <li>El desarrollo de actividades que generen un cambio mediante la interacción entre la experiencia
+                        previa y el nuevo conocimiento.
+                    </li>
+                    <li>Un aprendizaje que sirve para transversalizar lo aprendido en nuevas situaciones, por lo que más
+                        que memorizar hay que comprender.
+                    </li>
+                    <li>La vinculación de la teoría a la práctica mediante la experimentación.</li>
+                    <li>El desarrollo de la autonomía para que pueda surgir la creatividad, la espontaneidad, el
+                        liderazgo, la investigación y la observación.
+                    </li>
+                    <li>Contenidos temáticos que permiten el desarrollo de competencias vinculadas al entorno y a los
+                        intereses y necesidades de los estudiantes.
+                    </li>
+                    <li>La competencia de aprender a aprender.</li>
                 </ul>
+                <p className='parrafos text-gray-500 text-[13px]'>
+                    El Nuevo Colegio promueve una educación inclusiva, fundamentada en sus pilares filosóficos con el
+                    objetivo de formar ciudadanos integrales, competentes y emprendedores, en un ambiente de sana
+                    convivencia y de armonía con la naturaleza para la formación de niños y jóvenes felices,
+                    responsables, íntegros y autónomos.
+                </p>
             </div>
-        </div>
-        <div class="">
-            <h3>El Aprendizaje Significativo promueve:</h3>
-            <ul>
-                <li>El desarrollo de actividades que generen un cambio mediante la interacción entre la experiencia previa y el nuevo conocimiento.</li>
-                <li>Un aprendizaje que sirve para transversalizar lo aprendido en nuevas situaciones, por lo que más que memorizar hay que comprender.</li>
-                <li>La vinculación de la teoría a la práctica mediante la experimentación.</li>
-                <li>El desarrollo de la autonomía para que pueda surgir la creatividad, la espontaneidad, el liderazgo, la investigación y la observación.</li>
-                <li>Contenidos temáticos que permiten el desarrollo de competencias vinculadas al entorno y a los intereses y necesidades de los estudiantes.</li>
-                <li>La competencia de aprender a aprender.</li>
-            </ul>
-            <p className='parrafos'>El Nuevo Colegio promueve una educación inclusiva, fundamentada en sus pilares filosóficos con el objetivo de formar ciudadanos integrales, competentes y emprendedores, en un ambiente de sana convivencia y de armonía con la naturaleza para la formación de niños y jóvenes felices, responsables, íntegros y autónomos.</p>
-        </div>
-      </>
+        </>
     );
-  }
+}
