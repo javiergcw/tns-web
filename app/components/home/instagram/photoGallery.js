b49e3586f6ad7af68f059dcf4147ecf270bf91da
Javier Garcia Carrillo <50500251+javiergcw@users.noreply.github.com>
2024-02-11 23:12:00 -0500

    new design in instagram

--- app/components/home/instagram/photoGallery.js
+++ app/components/home/instagram/photoGallery.js
@@ -5,7 +5,16 @@ const PhotoGallery = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      // Tu código para buscar datos aquí...
+      const accessToken = 'IGQWRNRWQyZAUs4cG5IMFhmRVFQeGNQSmxYZADR4M1J0M0lsMnphcm43ZA29ENEZAfcDVjcVpaMW43RWZAmX0w2WkdoNUdDTVJzaEtNLThTSWNfdExURkJzOHVWbktRTlljUXdBN1JGeUNJTGhMaWpIaTc3TGZAHTzZAsSHMZD';
+      const apiURL = `https://graph.instagram.com/me/media?fields=thumbnail_url,media_url,caption,permalink&limit=80&access_token=${accessToken}`;
+
+      try {
+        const res = await fetch(apiURL);
+        const { data } = await res.json();
+        setMediaData(data.slice(0, 9)); // Solo guarda las primeras 9 imágenes
+      } catch (error) {
+        console.error("Error al obtener datos de Instagram:", error);
+      }
     };
 
     fetchData();
@@ -28,6 +37,7 @@ const PhotoGallery = () => {
     display: 'flex',
     justifyContent: 'center', // Centra la imagen horizontalmente
     alignItems: 'center', // Centra la imagen verticalmente
+    backgroundColor: 'white', // Fondo blanco para el contenedor
   };
 
   return (
