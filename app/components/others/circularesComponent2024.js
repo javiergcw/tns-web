509aba5490b69e7580d2daaf3986ce692e8b16f1
javiergcw <jotazeta001@gmail.com>
2024-04-18 21:35:18 -0500

    search bar in circular

--- app/components/others/circularesComponent2024.js
+++ app/components/others/circularesComponent2024.js
@@ -58,31 +58,92 @@ const data = [
 
 ];
 
+
+// Función para extraer el número de la circular del nombre
+const getNumberFromName = (name) => {
+    const match = name.match(/\d+/); // encuentra la primera secuencia de dígitos
+    return match ? parseInt(match[0], 10) : null;
+};
+
 const CircularList = () => {
     const [circularData, setCircularData] = useState([]);
+    const [filteredData, setFilteredData] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [sortDirection, setSortDirection] = useState('asc');
 
     useEffect(() => {
-        // Aquí deberías cargar tus datos desde el Excel.
         setCircularData(data);
+        setFilteredData(data);
     }, []);
+
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const handleSortChange = (event) => {
+        setSortDirection(event.target.value);
+    };
+
+    const filterAndSortData = () => {
+        let updatedData = circularData.filter(circular =>
+            circular.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+
+        updatedData.sort((a, b) => {
+            const numA = getNumberFromName(a.name);
+            const numB = getNumberFromName(b.name);
+            return sortDirection === 'asc' ? numA - numB : numB - numA;
+        });
+
+        setFilteredData(updatedData);
+    };
+
+    const clearFilters = () => {
+        setSearchTerm('');
+        setSortDirection('asc');
+        setFilteredData(circularData);
+    };
+
+    useEffect(() => {
+        filterAndSortData();
+    }, [searchTerm, sortDirection, circularData]);
+
     return (
         <div className="flex flex-col items-center justify-center p-10 w-full">
-            {circularData.map((circular, index) => (
-                <a
-                    key={index}
-                    href={circular.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={`w-11/12 md:w-3/4 xl:w-3/5 p-6 m-3 text-center text-white cursor-pointer rounded-lg transform hover:scale-105 transition duration-200 ease-in-out`}
-                    style={{
-                        backgroundColor: index % 2 === 0 ? '#679CCA' : '#6EB3F0',
-                        boxShadow: '0 2px 5px rgba(0, 0, 0, 0.2)',
-                        borderColor: index % 2 === 0 ? '#679CCA' : '#6EB3F0'
-                    }}
+            <div className="flex justify-center items-center mb-5 h-10">
+                <input
+                    type="text"
+                    placeholder="Buscar circular..."
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                    className="p-2 rounded-l-full border-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+             
+                <button
+                    onClick={clearFilters}
+                    className="h-10 bg-blue-500 text-white p-2 rounded-r-full hover:bg-blue-700 transition duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
                 >
-                    {circular.name}
-                </a>
-            ))}
+                    Limpiar filtros
+                </button>
+            </div>
+            <div className="flex flex-col items-center justify-center p-10 w-full transition-opacity duration-500 ease-in-out">
+                {filteredData.map((circular, index) => (
+                    <a
+                        key={index}
+                        href={circular.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className={`w-11/12 md:w-3/4 xl:w-3/5 p-6 m-3 text-center text-white cursor-pointer rounded-lg transform hover:scale-105 transition duration-200 ease-in-out`}
+                        style={{
+                            backgroundColor: index % 2 === 0 ? '#679CCA' : '#6EB3F0',
+                            boxShadow: '0 2px 5px rgba(0, 0, 0, 0.2)',
+                            borderColor: index % 2 === 0 ? '#679CCA' : '#6EB3F0'
+                        }}
+                    >
+                        {circular.name}
+                    </a>
+                ))}
+            </div>
         </div>
     );
 };
--- pages/communications/circulares.js
+++ pages/communications/circulares.js
@@ -9,59 +9,7 @@ import Circulares from '@/app/components/others/circularesComponent'
 import CircularList from '@/app/components/others/circularesComponent2024'
 
 const circulares = () => {
-    const enlaces = [
-        {
-            href: "https://drive.google.com/file/d/1MnfOcySR7VsoFA_Pa35ktoe5sXndX8Bz/view?usp=drive_link",
-            src: "/images/others/Group-10.png",
-            alt: "Group 10"
-        },
-        {
-            href: "https://drive.google.com/file/d/1htevKxxZBcxtePDrtYWvHATqFjLBvBtf/view?usp=drive_link",
-            src: "/images/others/Group-9.png",
-            alt: "Group 9"
-        },
-        {
-            href: "https://drive.google.com/file/d/1989mYYqU_M7gWSpGG478JgiO7bPMN4vA/view?usp=drive_link",
-            src: "/images/others/Group-8.png",
-            alt: "Group 8"
-        },
-        {
-            href: "https://drive.google.com/file/d/1E_S9R_li0lWkI5GBrmo1KxSVZs2tr99j/view?usp=drive_link",
-            src: "/images/others/Group-7-1.png",
-            alt: "Group 7 (1)"
-        },
-        {
-            href: "https://drive.google.com/file/d/1QsvOQLCD7RgRiSlDwblTEkXlg1Oiryvu/view?usp=drive_link",
-            src: "/images/others/Group-61.png",
-            alt: "Group 6"
-        },
-        {
-            href: "https://drive.google.com/file/d/1T_XPlUMHnHYBeTg8UBNDaKELm9wnQwpp/view?usp=drive_link",
-            src: "/images/others/Group-11.png",
-            alt: "Group 1"
-        },
-        {
-            href: "https://drive.google.com/file/d/1OJ2bODOiJj72EYA8k4_ZdMHDUCR30u-Z/view?usp=drive_link",
-            src: "/images/others/Group-41.png",
-            alt: "Group 4"
-        },
-        {
-            href: "https://drive.google.com/file/d/10osj00C85MEdqmXakUZ5nAkrjNRudpcD/view?usp=drive_link",
-            src: "/images/others/Group-3.png",
-            alt: "Group 3"
-        },
-        {
-            href: "https://drive.google.com/file/d/1E2hQNcwJO_n60F2KXxj2X86LOeGL9z4G/view?usp=drive_link",
-            src: "/images/others/Group-21.png",
-            alt: "Group 2"
-        },
-        {
-            href: "https://drive.google.com/file/d/1qQMzFZ7mbUBo5afJYPjqb7lA3ob2c9lF/view?usp=drive_link",
-            src: "/images/others/Group-51.png",
-            alt: "Group 5"
-        }
-    ];
-
+  
     return (
         <>
             <Navbar />
