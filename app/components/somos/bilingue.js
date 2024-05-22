1df771b00d114c7ff03b1b5d1c84dd17dc2120cd
javiergcw <jotazeta001@gmail.com>
2024-05-22 08:54:09 -0500

    others advances text esp/eng

--- app/components/somos/bilingue.js
+++ app/components/somos/bilingue.js
@@ -1,16 +1,31 @@
 import '/app/globals.css';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleLanguage } from '@/app/store/actions';
 
 export default function Bilingue() {
+    const isEnglish = useSelector((state) => state.isEnglish);
+    const dispatch = useDispatch();
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleOpen = (e) => {
+        e.preventDefault();
+        setIsOpen(!isOpen);
+    };
     return (
         <div className="backgroundImagefaith mt-4">
             <div className="contenedor flex justify-center items-center  py-8">
                 <div className="flex flex-col md:flex-row max-w-6xl mx-auto items-start">
-˝
+                    ˝
                     {/* Sección de Texto */}
                     <div className="md:w-1/2 text-center md:text-left px-4">
-                        <h1 className="text-3xl font-bold text-[#444444] mb-4">Methodology and didactics for sailors</h1>
+                        <h1 className="text-3xl font-bold text-[#444444] mb-4">
+                            {isEnglish ? "Methodology and didactics for sailors" : "Metodología y didáctica para navegantes"}
+
+                        </h1>
                         <p className="text-[13px]">
-                            Our curricular approach focused on BEING, DOING, and KNOWING, facilitates the development of generic, specific intercultural and bilingual competencies, which surely enrich holistic education, where subjects are intertwined with thematic lines of different visions, facilitating transversality and enriching the students' training process based on practice.
+                            {isEnglish ? "Our curricular approach focused on BEING, DOING, and KNOWING, facilitates the development of generic, specific intercultural and bilingual competencies, which surely enrich holistic education, where subjects are intertwined with thematic lines of different visions, facilitating transversality and enriching the students' training process based on practice." : "Nuestro enfoque curricular centrado en el SER, el HACER y el CONOCER, facilita el desarrollo de las competencias genéricas, las específicas y las interculturales y bilingües, las cuales con seguridad, enriquecen la formación holística, donde las asignaturas se entrelazan con líneas temáticas de diferentes visiones, facilitando la transversalidad y el enriquecimiento del proceso formativo de los estudiantes a partir de la práctica."}
                         </p>
                     </div>
 
