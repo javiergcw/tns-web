442d5f75d65f1d2f0b53e15ab8bd522ca4d6868b
javiergcw <jotazeta001@gmail.com>
2024-05-31 10:26:46 -0500

    hotfix: change code qr in admissions form

--- app/components/forms/formInscription.js
+++ app/components/forms/formInscription.js
@@ -3,14 +3,18 @@ const FormInscription = () => {
         <div className="px-4 md:px-8 lg:px-16 mb-8 mt-8">
             {/* Contenido para dispositivos móviles */}
             <div className="block md:hidden text-center">
-                <p>Escanea el siguiente código QR o haz clic aquí</p>
-                <img
-                    src="/images/others/qrform.png"
-                    alt="descarga"
-                    width="800"
-                    height="800"
-                    className="mx-auto"
-                />
+                <a href="https://forms.office.com/Pages/ResponsePage.aspx?id=GxPUyoHSe0KuXPduWcGPzWxbX51u9cdNlldIgWpMxGJUQVpMNjhDUzNLVzdZUDQ1TTFCUFoyRjNRVy4u&embed=true" target="_blank" rel="noopener noreferrer">
+                    Escanea el siguiente código QR o haz clic aquí
+                </a>
+                <a href="https://forms.office.com/Pages/ResponsePage.aspx?id=GxPUyoHSe0KuXPduWcGPzWxbX51u9cdNlldIgWpMxGJUQVpMNjhDUzNLVzdZUDQ1TTFCUFoyRjNRVy4u&embed=true" target="_blank" rel="noopener noreferrer">
+                    <img
+                        src="/images/others/qrform.png"
+                        alt="descarga"
+                        width="800"
+                        height="800"
+                        className="mx-auto"
+                    />
+                </a>
                 <center><b><span lang="es-CO">I voluntarily authorize THE NEW SCHOOL...</span></b></center>
 
             </div>
