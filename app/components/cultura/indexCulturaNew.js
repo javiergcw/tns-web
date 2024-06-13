ad722243b736968aefe9b217686f961850f625c5
Javier Garcia Carrillo <50500251+javiergcw@users.noreply.github.com>
2024-06-13 10:08:58 -0500

    New navbar design and layout implementation

--- app/components/cultura/indexCulturaNew.js
+++ app/components/cultura/indexCulturaNew.js
@@ -1,14 +1,11 @@
 
-
-
 import HeaderSomos from "@/app/components/home/headersomos";
 import Filosofia from "../somos/filosofia";
 import Formacion from "../somos/formacion";
 import Bilingue from "../somos/bilingue";
 import { Provider } from "react-redux";
 import store from '../../../app/store/store';
-import MainLayout from "../layouts/mainLayout";
-
+import MainLayout from "@/app/components/layouts/mainLayout";
 
 
 export default function IndexCulturaNew() {
--- app/components/educational/indexEducational.js
+++ app/components/educational/indexEducational.js
@@ -1,13 +1,9 @@
 
-
-import Navbar from "@/app/components/home/navbar";
-import FooterTwo from "@/app/components/home/footer/footerTwo";
-import Footer from "@/app/components/home/footer/footer";
 import HeaderSomos from "@/app/components/home/headersomos";
 import Cultura from "../somos/cultura";
 import { Provider } from "react-redux";
 import store from '../../../app/store/store';
-import MainLayout from "../layouts/mainLayout";
+import MainLayout from "@/app/components/layouts/mainLayout"
 
 
 export default function IndexEducational() {
