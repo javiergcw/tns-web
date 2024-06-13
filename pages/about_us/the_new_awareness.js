adb7fc8cfacd503992b14d13c183f4923b506bc0
Javier Garcia Carrillo <50500251+javiergcw@users.noreply.github.com>
2024-06-13 10:16:14 -0500

    New navbar design and layout implementation try 2

--- pages/about_us/the_new_awareness.js
+++ pages/about_us/the_new_awareness.js
@@ -3,7 +3,8 @@ import TheNewAwareness from '@/app/components/somos/the_new_awareness'
 import React from 'react'
 import { Provider } from "react-redux";
 import store from '../../app/store/store';
-import MainLayout from '@/app/components/layouts/mainLayout'
+import MainLayout from '../../app/components/layouts/mainLayout';
+
 
 const TheNewAwarenessView = () => {
     return (
