c8e5e70cab30af7ad7f31469525a684500624649
Javier Garcia <jotazeta001@gmail.com>
2024-07-08 14:50:16 -0500

    fix views and other features2

--- pages/create-product/index.js
+++ pages/create-product/index.js
@@ -1,25 +1,15 @@
 "use client";
 import "/app/globals.css";
 import React from "react";
-import Drawer from "@/app/components/others/drawer/drawer";
-import { useState, useEffect } from "react";
-import Container from "@/app/components/dashboard/container/container";
+import MainLayout from "@/app/components/layout/drawerLayout"; // Asegúrate de que la ruta sea correcta
 import CreatePurchaseForm from "@/app/components/others/container/createPurchaseForm";
 import ProveedorForm from "@/app/components/others/fields/proveedorForm";
 
 export default function CreateProduct() {
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-
-  const handleDrawerToggle = () => {
-    setIsDrawerOpen(!isDrawerOpen);
-  };
   return (
-    <div className="flex h-screen w-full overflow-hidden">
-      <Drawer isOpen={isDrawerOpen} onToggle={handleDrawerToggle} />
-      <Container isDrawerOpen={isDrawerOpen}>
-        <CreatePurchaseForm />
-        <ProveedorForm></ProveedorForm>
-      </Container>
-    </div>
+    <MainLayout>
+      <CreatePurchaseForm />
+      <ProveedorForm />
+    </MainLayout>
   );
 }
