d039f2d3c1e130bf709db65e939229cc2ffdbb64
Gabriel <73229789+Gabrielb2020@users.noreply.github.com>
2025-05-22 10:32:59 -0500

    nuevo cambio

--- app/components/home/navbarTest.js
+++ app/components/home/navbarTest.js
@@ -152,7 +152,7 @@ const Navbar = () => {
             </li>
             <li>
               <a
-                href="https://drive.google.com/file/d/1dzSs-L53VnDPK-W6CXruPyceSypgMf6m/view"
+                href="https://drive.google.com/file/d/1wa-S_MJV6JQLSQppLcfMHHxbJMFaR0x1/view?usp=sharing"
                 className="block py-2 px-3 text-blueButton rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 SCHEDULE
